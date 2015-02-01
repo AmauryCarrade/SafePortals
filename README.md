8d0f2036e0d28752bbf71676a35c2119f3e36b2e
Amaury Carrade <amaury@carrade.eu>
2015-02-01 19:21:24 +0100

    First commit - already with the main feature implemented.

---
+++ README.md
@@ -0,0 +1,5 @@
+# SafePortals
+
+Ensures a portal is always created inside the world border.
+
+Source code published under the Mozilla Public License.
---
+++ src/main/java/eu/carrade/amaury/SafePortals/PortalsListener.java
@@ -0,0 +1,49 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package eu.carrade.amaury.SafePortals;
+
+import org.bukkit.Location;
+import org.bukkit.TravelAgent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
+
+public final class PortalsListener implements Listener {
+	
+	/**
+	 * Used to move the portal inside the world border
+	 * 
+	 * @param ev
+	 */
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+	public void onPlayerPortal(PlayerPortalEvent ev) {
+		
+		ev.useTravelAgent(false);
+		
+		TravelAgent travelAgent = ev.getPortalTravelAgent();
+		
+		Location destination = travelAgent.findOrCreate(ev.getTo());
+		
+		// If Bukkit tries to link to a portal out of the border, we forces the
+		// generation of a new portal, inside this border.
+		// ev.getTo() is already the good location for this portal.
+		if(!SafePortalsUtils.isInsideBorder(destination)) {
+			travelAgent.createPortal(ev.getTo());
+			
+			// The getTo() location may not be safe.
+			Location safeTo = SafePortalsUtils.searchSafeSpot(ev.getTo());
+			if(safeTo != null) {
+				ev.setTo(safeTo);
+			}
+		}
+		
+		else {
+			ev.setTo(destination);
+		}
+	}
+}
