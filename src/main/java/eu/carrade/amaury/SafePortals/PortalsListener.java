df9c7cae748579cd42a7f62b49e432d21c641702
Amaury Carrade <amaury@carrade.eu>
2016-01-14 16:13:41 +0100

    Bumped the minimal version and refactored the whole thing.
    * NEW: updated the minimal Bukkit version to 1.8.3, as the WorldBorder API seems to be unavailable before now... (?!)
    * NEW: added missing JavaDocs.
    * BUG: fixed a minor bug in the safe spot finder.
    * RFT: refactored the code style.

--- src/main/java/eu/carrade/amaury/SafePortals/PortalsListener.java
+++ src/main/java/eu/carrade/amaury/SafePortals/PortalsListener.java
@@ -1,7 +1,6 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+ * the MPL was not distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 package eu.carrade.amaury.SafePortals;
@@ -13,33 +12,40 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerPortalEvent;
 
-public final class PortalsListener implements Listener {
-	
+
+public final class PortalsListener implements Listener
+{
+
 	/**
 	 * Used to move the portal inside the world border
-	 * 
+	 *
 	 * @param ev
 	 */
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
-	public void onPlayerPortal(PlayerPortalEvent ev) {
-		
+	@EventHandler (ignoreCancelled = true, priority = EventPriority.LOWEST)
+	public void onPlayerPortal(PlayerPortalEvent ev)
+	{
+
 		TravelAgent travelAgent = ev.getPortalTravelAgent();
 		Location destination = travelAgent.findPortal(ev.getTo());
-		
+
 		// If Bukkit tries to link to a portal out of the border, we forces the
 		// generation of a new portal, inside this border.
 		// ev.getTo() is already the good location for this portal.
-		if(!SafePortalsUtils.isInsideBorder(destination)) {
+		if (!SafePortalsUtils.isInsideBorder(destination))
+		{
 			ev.useTravelAgent(false);
 			Boolean success = travelAgent.createPortal(ev.getTo());
 
-			if(success) {
+			if (success)
+			{
 				ev.setTo(travelAgent.findPortal(ev.getTo()));
 
 				// The portal exact location returned by the portal agent may not be safe.
-				if (!SafePortalsUtils.isSafeSpot(ev.getTo())) {
+				if (!SafePortalsUtils.isSafeSpot(ev.getTo()))
+				{
 					Location safeTo = SafePortalsUtils.searchSafeSpot(ev.getTo());
-					if (safeTo != null) {
+					if (safeTo != null)
+					{
 						ev.setTo(safeTo);
 					}
 				}
--- src/main/java/eu/carrade/amaury/SafePortals/SafePortals.java
+++ src/main/java/eu/carrade/amaury/SafePortals/SafePortals.java
@@ -1,19 +1,18 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+ * the MPL was not distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 package eu.carrade.amaury.SafePortals;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SafePortals extends JavaPlugin {
-	
-	@Override
-	public void onEnable() {
-		
-		getServer().getPluginManager().registerEvents(new PortalsListener(), this);
-		
-	}
+
+public final class SafePortals extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        getServer().getPluginManager().registerEvents(new PortalsListener(), this);
+    }
 }
