167babf75e0db70b0076f04fd8c57f7d7763241a
Amaury Carrade <amaury@carrade.eu>
2016-01-14 16:29:06 +0100

    Fixed NPE when no portal is near a newly created portal. Fixes #1.
    * BUG: when the portal agent is not able to find a portal near the destination, it returns `null`, causing an NPE while trying to check if the destination is inside the border.

--- src/main/java/eu/carrade/amaury/SafePortals/PortalsListener.java
+++ src/main/java/eu/carrade/amaury/SafePortals/PortalsListener.java
@@ -15,41 +15,46 @@ import org.bukkit.event.player.PlayerPortalEvent;
 
 public final class PortalsListener implements Listener
 {
-
 	/**
 	 * Used to move the portal inside the world border
-	 *
-	 * @param ev
 	 */
 	@EventHandler (ignoreCancelled = true, priority = EventPriority.LOWEST)
 	public void onPlayerPortal(PlayerPortalEvent ev)
 	{
-
 		TravelAgent travelAgent = ev.getPortalTravelAgent();
 		Location destination = travelAgent.findPortal(ev.getTo());
 
-		// If Bukkit tries to link to a portal out of the border, we forces the
-		// generation of a new portal, inside this border.
-		// ev.getTo() is already the good location for this portal.
-		if (!SafePortalsUtils.isInsideBorder(destination))
+		if (destination != null)
 		{
-			ev.useTravelAgent(false);
-			Boolean success = travelAgent.createPortal(ev.getTo());
-
-			if (success)
+			// If Bukkit tries to link to a portal out of the border, we forces the
+			// generation of a new portal, inside this border.
+			// ev.getTo() is already the good location for this portal.
+			if (!SafePortalsUtils.isInsideBorder(destination))
 			{
-				ev.setTo(travelAgent.findPortal(ev.getTo()));
+				ev.useTravelAgent(false);
+				Boolean success = travelAgent.createPortal(ev.getTo());
 
-				// The portal exact location returned by the portal agent may not be safe.
-				if (!SafePortalsUtils.isSafeSpot(ev.getTo()))
+				if (success)
 				{
-					Location safeTo = SafePortalsUtils.searchSafeSpot(ev.getTo());
-					if (safeTo != null)
+					ev.setTo(travelAgent.findPortal(ev.getTo()));
+
+					// The portal exact location returned by the portal agent may not be safe.
+					if (!SafePortalsUtils.isSafeSpot(ev.getTo()))
 					{
-						ev.setTo(safeTo);
+						Location safeTo = SafePortalsUtils.searchSafeSpot(ev.getTo());
+						if (safeTo != null)
+						{
+							ev.setTo(safeTo);
+						}
 					}
 				}
 			}
+		}
+		else
+		{
+			// If no portal was found, then Bukkit will never link to a portal out of the border,
+			// as there isn't any portal for that. So we don't change anything.
+			ev.useTravelAgent(true);
 		}
 	}
 }
